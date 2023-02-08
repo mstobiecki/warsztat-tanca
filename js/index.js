1900bf14d5057361315a8540d2ff4d203bb51bd2
Mateusz Stobiecki <m100biecki@gmail.com>
2023-02-08 13:32:58 +0100

    added smooth scrolling in JS

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const yearEl = document.querySelector('.footer--js');
 const navigationButton = document.querySelector('.navigation__button');
 const headerNavigation = document.querySelector('.header');
+const allLinks = document.querySelectorAll('a:link');
 
 const year = () => {
 	const currentYear = new Date().getFullYear();
@@ -11,9 +12,34 @@ const mobileNavigation = () => {
 	headerNavigation.classList.toggle('navigation--js');
 };
 
-navigationButton.addEventListener('click', mobileNavigation);
+const scrollBehavior = () => {
+	allLinks.forEach((link) => {
+		link.addEventListener('click', function (e) {
+			e.preventDefault();
+
+			const href = link.getAttribute('href');
+
+			if (href === '#')
+				window.scrollTo({
+					top: 0,
+					behavior: 'smooth',
+				});
+
+			if (href !== '#' && href.startsWith('#')) {
+				const sectionEl = document.querySelector(href);
+				sectionEl.scrollIntoView({ behavior: 'smooth' });
+			}
+
+			if (link.classList.contains('navigation__link'))
+				headerNavigation.classList.toggle('navigation--js');
+		});
+	});
+};
 
 const init = () => {
 	year();
+	scrollBehavior();
 };
 init();
+
+navigationButton.addEventListener('click', mobileNavigation);
